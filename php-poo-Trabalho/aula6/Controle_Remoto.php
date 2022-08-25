ec76575d978a7a7a4e5ee18428a41a2f7bbfbe4f
Gustavo Sales Rodrigues <gustavo_sales2014@outlook.com>
2022-08-25 12:46:38 -0300

    teste
    x

---
+++ php-poo-Trabalho/aula6/Controle_Remoto.php
@@ -0,0 +1,85 @@
+<?php
+require_once 'controlador.php';
+    class ControleRemoto implements Controlador{
+
+    //Atributos
+    private $volume;
+    private $ligado;
+    private $tocando;
+    //Métodos Especiais
+    function _construct() {
+        $this->volume = 50;
+        $this->ligado = false;
+        $this->tocando = false;
+    }
+
+
+    #gets
+     function getVolume() {
+            return $this-> volume;
+     }
+    function getLigado() {
+            return $this-> ligado;
+    }
+    function getTocando() {
+            return$this-> tocando;
+    }
+
+
+#sets
+    function setVolume($volume) {
+        $this->volume = $volume;
+    }
+    function setLigado($ligado) {
+        $this->ligado = $ligado;
+    }
+    function setTocando($tocando) {
+        $this->tocando = $tocando;
+    }
+
+    //metodos 
+    public function ligar() {
+          $this->setLigado (true);
+    }
+    public function desligar() {
+            $this->setLigado (false);
+    }
+    public function abrirMenu() {
+            echo "<br>Está ligado?: " . ($this->getLigado()?"SIM":"NÃO");
+            echo "<br>Está tocando?: " . ($this->getTocando()?"SIM":"NÃO");
+            echo "<br>Volume: " . $this->getVolume();
+            for($i=0; $i <= $this->getVolume(); $i= $i + 10) {
+                echo "";
+            }
+            echo "<br>";
+            }
+    } 
+    public function fecharMenu() {
+         echo "<br>Fechando Menu...";
+    }
+    public function maisVolume() {  
+        if ($this->getLigado()){
+            $this->setVolume($this->getVolume() + 5);
+        }
+    }
+    public function menosVolume(){
+        if ($this->getLigado()) {
+            $this->setVolume($this->getVolume() - 5);
+        }
+    }
+    public function ligarMudo() {
+
+    }
+    public function desligarMudo() {
+
+    }
+    public function play() {
+
+    }
+    public function pause() {
+
+    }
+    }
+
+
+?>
---
+++ php-poo-Trabalho/aula6/index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Aula 6</title>
+    </head>
+<body>
+        <?php
+
+        require_once "Controlador.php";
+
+
+
+
+        ?>
+
+</body>
+
+    </html>
